05467121a41bb371e20f7a3d7d161cae192e4585
Bidyut kundu <106534797+bidyut10@users.noreply.github.com>
2023-04-14 21:50:57 +0530

    backend varriable

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ const db =require('./database/db')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://aquamarine-raindrop-7a1d70.netlify.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', routes);
 
 const PORT = process.env.PORT || 3000;
