187db9f109c5208035bf710e54056834dbaadf24
Bidyut kundu <106534797+bidyut10@users.noreply.github.com>
2023-04-14 22:36:31 +0530

    backend varriable

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://aquamarine-raindrop-7a1d70.netlify.app");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Origin', 'https://aquamarine-raindrop-7a1d70.netlify.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
+
 
 app.use('/', routes);
 
