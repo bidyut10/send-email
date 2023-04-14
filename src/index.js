4ac7c10054b33a19dc19b20ce407adbcee3cd7fa
Bidyut kundu <106534797+bidyut10@users.noreply.github.com>
2023-04-14 22:55:33 +0530

    backend varriable

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ const app = express();
 const routes = require('./routes/routes');
 const cors = require("cors");
 require("dotenv").config();
+const axios = require('axios');
 app.use(cors());
 const db =require('./database/db')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://aquamarine-raindrop-7a1d70.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 
 
 app.use('/', routes);
